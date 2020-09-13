8ad7f314019ed89f192375aced1e05a1a54620d1
Angelica Perez <aperez9423@gmail.com>
2020-09-13 19:43:23 -0400

    completed MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -20,9 +20,6 @@ const ColorList = ({ colors, updateColors }) => {
   };
 
   const saveEdit = e => {
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
     axiosWithAuth()
       .put(`/colors/${colorToEdit.id}`, colorToEdit)
       .then( res => {
